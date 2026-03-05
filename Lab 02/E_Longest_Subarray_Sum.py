d2d1e4a743c0c1d34d480dbeacafb6879c828a3e
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2026-03-06 00:19:49 +0600

    Create E_Longest_Subarray_Sum.py

---
+++ Lab 02/E_Longest_Subarray_Sum.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+
+left = 0
+current_sum = 0
+max_length = 0
+
+for right in range(N):
+    current_sum += arr[right]
+
+    while current_sum > K:
+        current_sum -= arr[left]
+        left += 1
+
+    max_length = max(max_length, right - left + 1)
+
+print(max_length)
